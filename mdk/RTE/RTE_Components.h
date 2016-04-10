ac890e25176418c5b9290bd7305ed2d887447281
Manuel Limones <limonesu.me@gmail.com>
2016-04-10 03:34:31 -0500

    Initial commit

---
+++ mdk/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'c_13_dac_same70' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "sam.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ source/main.c
@@ -0,0 +1,8 @@
+#include "sam.h"                        // Device header
+#include "watchdog_same70.h"
+
+int main(){
+    watchdog_disable();
+    while(1){
+    }
+}
